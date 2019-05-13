4607e5dd0f6ec25f8631c0e644a6716f54109c35
Ivan Salomon <49291080+IvanMarcosSalomon@users.noreply.github.com>
2019-05-13 20:50:12 -0300

    Mas correcciones a los test

--- Entrega_2_TPAterrizar/src/test/AerolineaTest.java
+++ Entrega_2_TPAterrizar/src/test/AerolineaTest.java
@@ -71,18 +71,18 @@ public class AerolineaTest {
 		vuelo1.agregarAsiento(asiento3);
 		//Asientos vuelo2
 		Asiento asiento4 = new Asiento(vuelo2, "P", "P", "R");
-		vuelo1.agregarAsiento(asiento4);
+		vuelo2.agregarAsiento(asiento4);
 		Asiento asiento5 = new Asiento(vuelo2, "E", "C", "R");
-		vuelo1.agregarAsiento(asiento5);
+		vuelo2.agregarAsiento(asiento5);
 		Asiento asiento6 = new Asiento(vuelo2, "T", "V", "D");
-		vuelo1.agregarAsiento(asiento6);
+		vuelo2.agregarAsiento(asiento6);
 		//Asientos vuelo3
 		Asiento asiento7 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento7);
+		vuelo3.agregarAsiento(asiento7);
 		Asiento asiento8 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento8);
+		vuelo3.agregarAsiento(asiento8);
 		Asiento asiento9 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento9);
+		vuelo3.agregarAsiento(asiento9);
 		//Agregar vuelos a aerolinea:
 		Aerolinea lanchita = new Aerolinea();
 		lanchita.agregarVuelo(vuelo1);
@@ -92,7 +92,7 @@ public class AerolineaTest {
 	}
 	
 	@Test
-	public void asientosDisponibles_seObtienenTresVelosQueCumplenConLaBusqueda() {
+	public void asientosDisponibles_seObtienenTresVuelosQueCumplenConLaBusqueda() {
 		String codDeVuelo1 = "EC0344";
 		String codDeVuelo2 = "TGX2";
 		String codDeVuelo3 = "JAH18";
@@ -156,18 +156,18 @@ public class AerolineaTest {
 		vuelo1.agregarAsiento(asiento3);
 		//Asientos vuelo2
 		Asiento asiento4 = new Asiento(vuelo2, "P", "P", "R");
-		vuelo1.agregarAsiento(asiento4);
+		vuelo2.agregarAsiento(asiento4);
 		Asiento asiento5 = new Asiento(vuelo2, "E", "C", "R");
-		vuelo1.agregarAsiento(asiento5);
+		vuelo2.agregarAsiento(asiento5);
 		Asiento asiento6 = new Asiento(vuelo2, "T", "V", "D");
-		vuelo1.agregarAsiento(asiento6);
+		vuelo2.agregarAsiento(asiento6);
 		//Asientos vuelo3
 		Asiento asiento7 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento7);
+		vuelo3.agregarAsiento(asiento7);
 		Asiento asiento8 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento8);
+		vuelo3.agregarAsiento(asiento8);
 		Asiento asiento9 = new Asiento(vuelo3, "T", "C", "D");
-		vuelo1.agregarAsiento(asiento9);
+		vuelo3.agregarAsiento(asiento9);
 		//Agregar vuelos a aerolinea:
 		Aerolinea lanchita = new Aerolinea();
 		lanchita.agregarVuelo(vuelo1);
