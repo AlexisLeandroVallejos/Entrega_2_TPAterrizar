7dfbd55d22a40801177905e7c44cbd52a4ac4e93
Ivan Salomon <ivanmsalomon@hotmail.com>
2019-06-20 15:15:42 -0300

    Creado el teste Controller

---
+++ testModelo/vistaTest/ControllerTest.java
@@ -0,0 +1,15 @@
+package vistaTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import modelo.Aerolinea;
+import modelo.UsuarioEstandar;
+import vista.AterrizarPrincipal;
+
+public class ControllerTest {
+
+
+	
+}
